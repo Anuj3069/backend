dd0e907a785a683230165b2ed804d44657a12dfe
deepak96999 <anujgupta596@gmail.com>
2023-11-22 01:30:38 +0530

    Custom api response and error handling

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 //require ('dotenv').config()
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
-
+import { app } from "./app.js";
 dotenv.config({
     path: './env'
 })
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000)
+    console.log(`server is running at port: ${process.env.PORT}`)
+})
+.catch((err)=>{
+    console.log("MONGO DB connectionb failed !!! ",err)
+})
